27240593b729a3c19aaf871a8fce021c6ab09388
ecerrahoglu <erimcrhkn00@gmail.com>
2022-12-11 14:32:37 +0300

    added pyinstaller to requirements.txt and updated runTool.sh

--- runTool.sh
+++ runTool.sh
@@ -1,22 +1,42 @@
 #!/usr/bin/bash
 
-if [[ "$VIRTUAL_ENV" == "" ]]
-then
-  pip install virtualenv
-  virtualenv venv
-  source venv/bin/activate
-  pip install -r requirements.txt
-fi
-source venv/bin/activate
-pip install -r requirements.txt
-
 if which python3 &>/dev/null; then
-  python3 main.py
+  if which virtualenv > /dev/null; then
+    # virtualenv is installed, so do something
+    virtualenv venv
+    . venv/bin/activate
+    pip install -r requirements.txt
+    python3 main.py
+    exit 1
+  else
+    # virtualenv is not installed, so do something else
+    pip install virtualenv
+    virtualenv venv
+    . venv/bin/activate
+    pip install -r requirements.txt
+    python3 main.py
+    exit 1
+  fi
 else
   if which python &>/dev/null; then
-    python main.py
+    if which virtualenv > /dev/null; then
+      # virtualenv is installed, so do something
+      virtualenv venv
+      . venv/bin/activate
+      pip install -r requirements.txt
+      python main.py
+      exit 1
+    else
+      # virtualenv is not installed, so do something else
+      pip install virtualenv
+      virtualenv venv
+      . venv/bin/activate
+      pip install -r requirements.txt
+      python main.py
+      exit 1
+    fi
   else
-    echo "Error: Python is not installed on this system"
+    echo "Error: Python is not installed on this system, please install python 3.X.X on your system!"
     exit 1
   fi
 fi
